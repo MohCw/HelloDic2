b87cf07888d9c036f0e652fb5c967bf839afcdd1
MohCw <mohamedcamaraj@gmail.com>
2025-01-04 09:36:28 +0000

    Creation de l'entite Etudiant

--- src/main/java/sn/edu/ept/git/entities/Etudiant.java
+++ src/main/java/sn/edu/ept/git/entities/Etudiant.java
@@ -1,4 +1,15 @@
 package sn.edu.ept.git.entities;
 
-public class Etudiant {
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+import java.io.Serializable;
+
+@Entity
+public class Etudiant implements Serializable {
+    @Id
+    private Integer id;
+    private String nom;
+    private String prenom;
+    private String adresse;
 }
